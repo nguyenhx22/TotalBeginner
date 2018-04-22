c7a20c934b37eea9fcd8a35e102efd69d2142fa7
Hien <hienxnguyen@gmail.com>
2018-04-21 22:56:04 -0600

    Initial commit of TotalBeginner with Person class.

---
+++ TotalBeginner/src/org/totalbeginner/tutorial/Person.java
@@ -0,0 +1,35 @@
+package org.totalbeginner.tutorial;
+
+public class Person {
+	
+	//Fields
+	private String name;        //name of person
+	private int maximumBooks;   //Max books person can check out
+	
+	//Constructor
+	public Person() {
+		name = "unknown name";
+		maximumBooks = 3;		
+	}
+	
+	public String getName()	{
+		return name;
+	};
+	
+	public void setName(String anyName) {
+		name = anyName;
+	}
+	
+	public void setMaximumBooks(int maximumBooks) {
+		this.maximumBooks = maximumBooks;
+	}
+	
+	public int getMaximumBooks() {
+		return maximumBooks;
+		}
+	
+	public String toString() {
+		return this.getName() + " (" + this.getMaximumBooks() +
+		" books)";
+		}
+}
